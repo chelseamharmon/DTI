0f90251a2e7b733399e614192c8beb3df4eba630
Chelsea Harmon <cmh2228@columbia.edu>
2020-06-24 14:03:07 -0700

    added 3 tracts (changed from length 6 to length 9)

--- 6.2extractingAllValues.R
+++ 6.2extractingAllValues.R
@@ -4,7 +4,7 @@ tractList <- read.table("../../autoptx/preproc/tractList.txt")
 names(tractList) <- "tracts"
 names(subjectList) <- "SUB_ID"
 
-subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],6), l=1:6, r=1:6, tract=1:6)
+subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],9), l=1:9, r=1:9, tract=1:9)
 
 for (i in 1:nrow(subjectList)){
 	for (ii in 1:length(tractList$tracts)){
@@ -19,17 +19,17 @@ for (i in 1:nrow(subjectList)){
 		             subList$tract[subList$tract==ii] <- paste0(tractList$tracts[ii], "_FA")
 	}
 	j <- i +1
-	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],6), l=1:6, r=1:6, tract=1:6)
+	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],9), l=1:9, r=1:9, tract=1:9)
 	subList <- rbind(subList, subListTemp)
 }
 
-allTracts_FA <-(subList[1:(nrow(subList)-6),])
+allTracts_FA <-(subList[1:(nrow(subList)-9),])
 
 
 write.csv(allTracts_FA, file="../../autoptx/visualise/allTracts_FA0.005.csv")
 
 
-subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],6), l=1:6, r=1:6, tract=1:6)
+subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],9), l=1:9, r=1:9, tract=1:9)
 
 for (i in 1:nrow(subjectList)){
 	for (ii in 1:length(tractList$tracts)){
@@ -44,17 +44,17 @@ for (i in 1:nrow(subjectList)){
 		             subList$tract[subList$tract==ii] <- paste0(tractList$tracts[ii], "_MD")
 	}
 	j <- i +1
-	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],6), l=1:6, r=1:6, tract=1:6)
+	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],9), l=1:9, r=1:9, tract=1:9)
 	subList <- rbind(subList, subListTemp)
 }
 
-allTracts_MD <-(subList[1:(nrow(subList)-6),])
+allTracts_MD <-(subList[1:(nrow(subList)-9),])
 
 
 write.csv(allTracts_MD, file="../../autoptx/visualise/allTracts_MD0.005.csv")
 
 
-subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],6), l=1:6, r=1:6, tract=1:6)
+subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],9), l=1:9, r=1:9, tract=1:9)
 
 for (i in 1:nrow(subjectList)){
 	for (ii in 1:length(tractList$tracts)){
@@ -69,17 +69,17 @@ for (i in 1:nrow(subjectList)){
 		             subList$tract[subList$tract==ii] <- paste0(tractList$tracts[ii], "_AD")
 	}
 	j <- i +1
-	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],6), l=1:6, r=1:6, tract=1:6)
+	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],9), l=1:9, r=1:9, tract=1:9)
 	subList <- rbind(subList, subListTemp)
 }
 
-allTracts_AD <-(subList[1:(nrow(subList)-6),])
+allTracts_AD <-(subList[1:(nrow(subList)-9),])
 
 
 write.csv(allTracts_AD, file="../../autoptx/visualise/allTracts_AD0.005.csv")
 
 
-subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],6), l=1:6, r=1:6, tract=1:6)
+subList  <-  data.frame(SUB_ID=rep(subjectList$SUB_ID[1],9), l=1:9, r=1:9, tract=1:9)
 
 for (i in 1:nrow(subjectList)){
 	for (ii in 1:length(tractList$tracts)){
@@ -94,11 +94,11 @@ for (i in 1:nrow(subjectList)){
 		             subList$tract[subList$tract==ii] <- paste0(tractList$tracts[ii], "_RD")
 	}
 	j <- i +1
-	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],6), l=1:6, r=1:6, tract=1:6)
+	subListTemp <- data.frame(SUB_ID=rep(subjectList$SUB_ID[j],9), l=1:9, r=1:9, tract=1:9)
 	subList <- rbind(subList, subListTemp)
 }
 
-allTracts_RD <-(subList[1:(nrow(subList)-6),])
+allTracts_RD <-(subList[1:(nrow(subList)-9),])
 
 
 write.csv(allTracts_RD, file="../../autoptx/visualise/allTracts_RD0.005.csv")
